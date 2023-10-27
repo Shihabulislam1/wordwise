5f6fc2492a890af8f006124518981b2cf7225cab
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2023-10-27 12:02:09 +0600

    starting:

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,14 @@
+import AppNav from "../components/AppNav";
+import PageNav from "../components/PageNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <div>App Layout</div>
+    </div>
+  );
+}
+
+export default AppLayout;
